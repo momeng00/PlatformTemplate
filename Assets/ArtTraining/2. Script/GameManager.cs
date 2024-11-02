0e96e59680f7059aef611cf1c0216d29cd06d11d
momeng00 <69623137+momeng00@users.noreply.github.com>
2024-11-02 20:05:29 +0900

    temp
    temp

--- Assets/ArtTraining/2. Script/GameManager.cs
+++ Assets/ArtTraining/2. Script/GameManager.cs
@@ -27,6 +27,5 @@ public class GameManager : MonoBehaviour
     public void GetGamePoint()
     {
         forGameClear++;
-        Debug.Log("Up");
     }
 }
--- Assets/ArtTraining/2. Script/PlatformObj/Switch/BlockForStage2.cs
+++ Assets/ArtTraining/2. Script/PlatformObj/Switch/BlockForStage2.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public enum BlockState
 {
@@ -10,7 +12,11 @@ public enum BlockState
 public class BlockForStage2 : Switchable, IReset
 {
     private Animator ani;
+    public TMP_Text text;
+    public Vector2 textPos;
+    public string textValue;
     private BoxCollider2D col;
+    bool isWork;
     public void Reset()
     {
         ChangeState(BlockState.Off);
@@ -19,21 +25,20 @@ public class BlockForStage2 : Switchable, IReset
     // Start is called before the first frame update
     void Start()
     {
-        col=GetComponent<BoxCollider2D>();
+        isWork = false;
+        col =GetComponent<BoxCollider2D>();
         ani=GetComponent<Animator>();
+        ChangeState(BlockState.Off);
+        if (text != null)
+        {
+            text.GetComponent<RectTransform>().position = Camera.main.WorldToScreenPoint(textPos);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            SwitchOn();
-        }
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            SwitchOff();
-        }
+        
     }
     public override void SwitchOn()
     {
@@ -50,14 +55,46 @@ public class BlockForStage2 : Switchable, IReset
         switch (state)
         {
             case BlockState.On:
+                if (text != null)
+                {
+                    text.text = "";
+                    StartCoroutine(Typing());
+                }
                 ani.SetBool("isOn", true);
                 col.enabled = true;
                 break;
             case BlockState.Off:
+                if (text != null)
+                {
+                    isWork = false;
+                    text.text = "";
+                }
                 ani.SetBool("isOn",false);
                 col.enabled = false;
                 break;
 
         }
+    }
+
+    IEnumerator Typing()
+    {
+        if (!isWork) {
+            isWork= true;
+            yield return new WaitForSeconds(0.7f);
+            while (isWork)
+            {
+                foreach (char c in textValue.ToCharArray())
+                {
+                    if (!isWork)
+                    {
+                        break;
+                    }
+                    text.text += c;
+                    yield return new WaitForSeconds(0.2f);
+                }
+                isWork = false;
+            }
+        }
+        yield return null;
     }
 }
