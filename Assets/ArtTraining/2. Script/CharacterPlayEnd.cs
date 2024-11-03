8860af0670c4d67a07dc378ea2e1168f21898c80
momeng00 <69623137+momeng00@users.noreply.github.com>
2024-11-03 11:56:27 +0900

    Ending
    Create Ending Scene .

--- Assets/ArtTraining/2. Script/CharacterPlayEnd.cs
+++ Assets/ArtTraining/2. Script/CharacterPlayEnd.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterPlayEnd : MonoBehaviour
 {
@@ -10,24 +11,47 @@ public class CharacterPlayEnd : MonoBehaviour
     public Rigidbody2D rb;
     public float horizontal;
     public Animator ani;
-    [SerializeField]private Collider2D[] Trigger;
+    public bool isRun;
+    public GameObject Player;
+    public RectTransform[] UIWidth;
+    public RectTransform[] UIHeight;
 
     private void Start()
     {
+        if (isMovable)
+        {
+            StartCoroutine("ExpandWidthCoroutine");
+            StartCoroutine("ExpandHeightCoroutine");
+        }
+        isRun=false;
         rb = GetComponent<Rigidbody2D>();
         ani = GetComponent<Animator>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        foreach (var trigger in Trigger) 
+        switch (collision.name)
         {
-            if(collision == trigger)
-            {
-                Debug.Log(trigger.name);
-            }
+            case "First":
+                break;
+            case "Second":
+                break;
+            case "Third":
+                isRun = true;
+                moveSpeed = 8.0f;
+                break;
+            case "Fourth":
+                isRun = false;
+                moveSpeed = 3.0f;
+                break;
         }
     }
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject == Player)
+        {
+            SceneManager.LoadSceneAsync("Ending");
+        }
+    }
     public virtual void Move()
     {
         rb.position += move * Time.fixedDeltaTime;
@@ -38,11 +62,22 @@ public class CharacterPlayEnd : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         if (horizontal !=0)
         {
-            ani.SetBool("isWalkWalk",true);
+            if (isRun)
+            {
+                ani.SetBool("isWalk", true);
+                ani.SetBool("isWalkWalk", false);
+            }
+            else
+            {
+                ani.SetBool("isWalkWalk", true);
+                ani.SetBool("isWalk", false);
+            }
+            
         }
         else
         {
             ani.SetBool("isWalkWalk", false);
+            ani.SetBool("isWalk", false);
         }
         if (isMovable)
         {
@@ -52,5 +87,57 @@ public class CharacterPlayEnd : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
+    }
+
+
+    IEnumerator ExpandWidthCoroutine()
+    {
+        float initialWidth;
+        float targetWidth;
+        float addWidth;
+        while (true)
+        {
+            if (horizontal == 0){
+                addWidth = 0f;
+            }
+            else
+            {
+                addWidth = horizontal > 0 ? 5f : -5f;
+            }
+            foreach (RectTransform v in UIWidth)
+            {
+                initialWidth = v.sizeDelta.x;
+                targetWidth = initialWidth + addWidth;
+               
+                v.sizeDelta = new Vector2(targetWidth, v.sizeDelta.y);
+            }
+            yield return new WaitForSeconds(0.3f);
+        }
+        
+    }
+    IEnumerator ExpandHeightCoroutine()
+    {
+        float initialHeight;
+        float targetHeight;
+        float addHeight;
+        while (true)
+        {
+            if (horizontal == 0)
+            {
+                addHeight = 0f;
+            }
+            else
+            {
+                addHeight = horizontal > 0 ? 3f : -3f; ;
+            }
+            foreach (RectTransform v in UIHeight)
+            {
+                initialHeight = v.sizeDelta.y;
+                targetHeight = initialHeight + addHeight;
+                v.sizeDelta = new Vector2(v.sizeDelta.x, targetHeight);
+            }
+            yield return new WaitForSeconds(0.3f);
+        }
+
     }
 }
